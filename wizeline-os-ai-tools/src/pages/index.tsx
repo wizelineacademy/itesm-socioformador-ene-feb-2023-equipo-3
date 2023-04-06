2ca77c21cec91fd76388b786e5fe1b1eb7b4c934
Daira Rodriguez <35277731+dairadriana@users.noreply.github.com>
2023-04-05 19:17:17 -0600

    Avance Tabla Colabs
    Ya se tiene el componente, voy a hacer el css, cambiar el formato de skills
    Cambiar el estilo en general

--- wizeline-os-ai-tools/src/pages/index.tsx
+++ wizeline-os-ai-tools/src/pages/index.tsx
@@ -2,23 +2,19 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
+import ColabTable from '../components/ColabTable'
 
 import { api } from "@/utils/api";
 
+const data = [ 
+  {employee_name: 'Joaquín Bravo', avatar: "../../public/avatars/example_1.jpg", role: "Senior, Technology Manager", skills: "Python, Drupal, PHP"},
+];
+
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Wizeline OS</title>
-        <meta name="Wizeline OS" content="Sign In" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="">
-        <div className="bg-orange-500">
-          Hello World!
-        </div>
-      </main>
-    </>
+    <div>
+      <ColabTable data = {data} />
+    </div>
   );
 };
 
