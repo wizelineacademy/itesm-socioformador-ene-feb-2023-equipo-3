145dddbfc8b93d58ec6291379023de603cc06b50
Cristina Hernandez <A00830775@tec.mx>
2023-04-13 12:37:49 -0600

    Update Skill.tsx

--- wizeline-os-ai-tools/src/components/Skill.tsx
+++ wizeline-os-ai-tools/src/components/Skill.tsx
@@ -13,7 +13,19 @@ export const skills = [{
     skill: 'C#'
 }, {
     id: 3,
-    skill: 'Throwing a Ball'
+    skill: 'C++'
+},{
+    id: 4,
+    skill: 'Leadership'
+}, {
+    id: 5,
+    skill: 'Javascript'
+}, {
+    id: 6,
+    skill: 'CSS'
+}, {
+    id: 7,
+    skill: 'SQL'
 }];
 
 export function Skill(){
@@ -30,14 +42,18 @@ export function Skill(){
 export default function MySkills() {
     const listItems = skills.map(skill => 
         <div key={skill.id}>
-            <span className='bg-sky-500 text-white font-semibold rounded p-2 m-2'> { skill.skill } </span>
+            <span className='bg-sky-500 text-white font-semibold rounded p-2 mr-3 '> { skill.skill } </span>
         </div>
         );
     return (
-        <section className='flex flex-row bg-white'>
-            <h1>Skills</h1>
-            {listItems}
-        </section>
-        
+        <div>
+            <div className="flex flex-row bg-white">
+                <h1 className="font-inter text-2xl font-semibold mb-3">Skills</h1>
+            </div>
+            <div className="pt-1 center-content flex flex-row bg-white ">
+                {listItems}
+            </div>
+        </div>
+
     );
 }
