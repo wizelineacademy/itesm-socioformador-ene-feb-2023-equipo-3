2cba302d13c8edbd84976d5c2e7199413c03042f
Daira Rodriguez <35277731+dairadriana@users.noreply.github.com>
2023-04-14 12:10:46 -0600

    Update index.tsx

--- wizeline-os-ai-tools/src/pages/index.tsx
+++ wizeline-os-ai-tools/src/pages/index.tsx
@@ -1,18 +1,6 @@
-import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-import ColabTable from '../components/ColabTable'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Certificate } from 'crypto'
 
-import { api } from "@/utils/api";
-
-
-const Home: NextPage = () => {
-  return (
-    <div>
-      <ColabTable/>
-    </div>
-  );
-};
-
-export default Home;
+export default function Home() {
+}
