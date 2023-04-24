473974fe4f3a44265cfea13cc5275528eb7ad9f7
Carmina López <a00830265@itesm.mx>
2023-04-24 12:35:33 -0600

    Tabla de Projects
    Se creo componente de tabla de Projects.

---
+++ wizeline-os-ai-tools/src/components/Projects.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
+export const projects = [{
+    id: 0,
+    name: 'Arcxp',
+    company: 'Washington Post',
+    state: 'Washington',
+    country: 'United States'
+}, {
+    id: 1,
+    name: 'CRM System Enhancement',
+    company: 'ABC Software Development',
+    state: 'New York',
+    country: 'United States'
+}, {
+    id: 2,
+    name: 'Data Analytics Platform Implementation',
+    company: 'Tech Innovators',
+    state: 'Madrid',
+    country: 'Spain'
+}, {
+    id: 3,
+    name: 'Artificial Intelligence Chatbot Development',
+    company: 'ABC Software Co.',
+    state: 'Beijing',
+    country: 'China'
+}, {
+    id: 4,
+    name: 'Multi-platform Mobile Wallet Development',
+    company: 'XYZ Software Solutions',
+    state: 'Quebec',
+    country: 'United States' 
+}];
+
+interface Data {
+    id: number;
+    name: string; 
+    company: string;
+    state: string;
+    country: string;
+}
+  
+interface Props {
+    data: Data[];
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    avatar: {
+        width: theme.spacing(6.5),
+        height: theme.spacing(6.5),
+        borderRadius: '50%',
+        border: `2px solid ${theme.palette.common.white}`,
+      },
+  }),
+);
+
+
+const ProjectItem: React.FC<Props> = ({ data }) => {
+    const classes = useStyles();
+
+    return (
+        <div className= "relative overflow-x-auto shadow-xl">
+            <table className="w-full text-left">
+                <thead className="pl-20 bg-white border border-gray-200">
+                    <tr>
+                        <th scope="col" className= "px-6 text-base font-inter font-normal py-4">
+                            Projects
+                        </th>
+                        <th scope="col"></th>
+                    </tr> 
+
+                </thead>
+                <tbody>
+                {data.map((project) => (
+                    <tr key={project.id} className="bg-white border border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td width = {50} className = "px-6">
+                            <Avatar
+                                alt={project.company}
+                                src='../../public/avatars/example_1.jpg'
+                                className={classes.avatar}
+                            />
+                        </td>
+                        <th className="py-4">
+                            <p className="font-inter text-lg font-semibold text-black">{project.name}</p>
+                            <p className="font-inter text-base font-light">{project.company}</p>
+                            <p className="font-inter text-sm font-normal text-[#878787]">{project.state}, {project.country}</p>
+                        </th>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+      
+
+export default function Project() {
+  return (
+    <div>
+        <ProjectItem data = {projects} />
+    </div>
+  )
+}
