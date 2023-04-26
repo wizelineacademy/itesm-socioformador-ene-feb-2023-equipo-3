34218f676b9ae588a698b7382b4edafceeaf44af
Carmina López <a00830265@itesm.mx>
2023-04-26 10:20:05 -0600

    Componente Vacante

---
+++ wizeline-os-ai-tools/src/pages/borrar.tsx
@@ -0,0 +1,9 @@
+import Vacants from '@/components/Vacants'
+
+export default function newUser() {
+    return(
+        <div className= "w-96 m-5">
+            <Vacants></Vacants>
+        </div>
+    )
+}
