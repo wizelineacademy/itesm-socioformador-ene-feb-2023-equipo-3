584f179cf372907dd67edd14e646a7b34f09bdbb
Cristina Hernandez <A00830775@tec.mx>
2023-04-26 10:22:34 -0600

    Se movieron cambios a pagina de profile

--- wizeline-os-ai-tools/src/pages/profile.tsx
+++ wizeline-os-ai-tools/src/pages/profile.tsx
@@ -2,19 +2,34 @@ import GeneralInfo from '@/components/GeneralInfo'
 import About from '@/components/About'
 import PastWork from '@/components/PastWork'
 import Skills from '@/components/Skill'
-import Certification from '@/components/Certification'
+import Certifications from '@/components/Certification'
+import { db } from '@/server/db'
 import Head from 'next/head'
 import Image from 'next/image'
 import AIAssitant from '@/components/AIAssistant'
 import Education from '@/components/Education'
 
-export default function Home() {
+
+export async function getStaticProps() {
+      const allCerts = await db.certification.findMany()
+      const allEdus = await db.education.findMany()
+
+      
+      return {
+          props: {
+            certifications: allCerts,
+            educations: allEdus, 
+          },
+      }
+  }
+
+export default function Home(props: any) {
   return (
     <>
       <GeneralInfo />
       <div className="lg:flex divide-x">
         <div className="flex-initial w-3/4 pl-20 grid grid-cols-1 divide-y divide-x-reverse">
-          <span></span>
+          <span></span>s
           <div className="pt-7 pb-5 pr-7">
             <About />
           </div>
@@ -25,13 +40,13 @@ export default function Home() {
             <Skills />
           </div>
           <div className="pt-7 pb-7 pr-7">
-            <Education />
+            <Education props={props}/>
           </div>
         </div>
-        <div className="flex-initial w-1/4  mr-20 divide-y">
+        <div className="flex-initial w-1/4 mr-20 divide-y">
           <span></span>
           < div className="pl-5 pt-7 ">
-            <Certification />
+          <Certifications props={props}/>
           </div>
         </div>
       </div>
