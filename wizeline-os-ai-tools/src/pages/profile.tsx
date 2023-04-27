46282008f0e3ee4f084ab5cd0580944c3e70991f
Carmina López <a00830265@itesm.mx>
2023-04-26 20:34:33 -0600

    Conexion de About con DB
    Se agregó la conexión inicial con la base de datos.

--- wizeline-os-ai-tools/src/pages/profile.tsx
+++ wizeline-os-ai-tools/src/pages/profile.tsx
@@ -7,9 +7,60 @@ import Head from 'next/head'
 import Image from 'next/image'
 import AIAssitant from '@/components/AIAssistant'
 import Education from '@/components/Education'
+import { db } from '@/server/db'
 
-export default function profile() {
+export default function profile(props: any) {
+  console.log(props.About)
   return (
+    <>
+      <GeneralInfo />
+      <div className="lg:flex divide-x">
+        <div className="flex-initial w-3/4 pl-20 grid grid-cols-1 divide-y divide-x-reverse">
+          <span></span>
+          <div className="pt-7 pb-5 pr-7">
+            <About props={props.About}/>
+          </div>
+          <div className="pr-7 pb-7">
+            <PastWork />
+          </div>
+          <div className="pt-7 pb-7">
+            
+          </div>
+          <div className="pt-7 pb-7 pr-7">
+            
+          </div>
+        </div>
+        <div className="flex-initial w-1/4  mr-20 divide-y">
+          <span></span>
+          < div className="pl-5 pt-7 ">
+            
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+
+  const about = await db.about_me.findUnique({
+    where: {
+      id_employee: 1 //Aquí se pone el id de la persona que inició sesión
+    },
+   select: {
+     description: true
+  }
+  });
+
+  return {
+      props: {
+        About: about, 
+      },
+  }
+}
+
+
+/*
     <>
       <GeneralInfo />
       <div className="lg:flex divide-x">
@@ -36,6 +87,10 @@ export default function profile() {
         </div>
       </div>
     </>
-  )
-}
 
+*/
+
+/*
+
+
+*/
