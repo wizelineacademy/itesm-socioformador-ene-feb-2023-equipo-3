f6c393363f670f665e9debaea836107dd0ffe605
Carmina López <74677482+carminalp@users.noreply.github.com>
2023-06-15 11:27:27 -0600

    correccion url fetch

--- wizeline-os-ai-tools/src/pages/profile.tsx
+++ wizeline-os-ai-tools/src/pages/profile.tsx
@@ -20,7 +20,7 @@ export default function Profile() {
   // Fetching data from API
   useEffect(() => {
     setLoading(true);
-    fetch("http://equipo3.labs.wizeline.io/api/getUser")
+    fetch("http://equipo3.labs.wizeline.io:3000/api/getUser")
       .then((res) => res.json())
       .then((data) => {
         setData(data);
